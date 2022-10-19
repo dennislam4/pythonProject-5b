56744e02fd875b214d01b989c45852117beb724c
Dennis <lamden@oregonstate.edu>
2022-10-19 15:22:24 -0600

    changing surnames to surname

--- NobelData.py
+++ NobelData.py
@@ -26,7 +26,7 @@ class NobelData:
             prize_catagory = self.nobel_data["prizes"][index]["category"]
 
             if prize_year == year and prize_catagory == catagory:
-                surnames_of_winners.append(self.nobel_data["surname"][index]["laureates"])
+                surnames_of_winners.append(self.nobel_data["laureates"][index]["surname"])
             pass
 
         # for index in range(0, len(self.nobel_data["winners"])):
