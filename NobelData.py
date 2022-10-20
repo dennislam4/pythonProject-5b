5fc4b92c03669f5bdba8db90b1efe8b0cd856011
Dennis <lamden@oregonstate.edu>
2022-10-20 13:39:04 -0600

    removing index

--- NobelData.py
+++ NobelData.py
@@ -20,12 +20,12 @@ class NobelData:
         """
         surnames_of_winners = []
 
-        for index in self.nobel_data["prizes"]:
-            prize_year = self.nobel_data["prizes"][index]["year"]
-            prize_catagory = self.nobel_data["prizes"][index]["category"]
+        for nobel in self.nobel_data["prizes"]:
+            prize_year = self.nobel_data["prizes"][nobel]["year"]
+            prize_catagory = self.nobel_data["prizes"][nobel]["category"]
 
             if prize_year == year and prize_catagory == catagory:
                 winners = (self.nobel_data["prizes"])
-                surnames_of_winners.append(winners[index]["surname"])
+                surnames_of_winners.append(winners[nobel]["surname"])
             break
         return surnames_of_winners.sort()
